0cfa234e33fb82f2ce6de46706ec7d843c9c4d81
ufofamily <60894835+ufofamily@users.noreply.github.com>
2025-07-13 00:36:52 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,20 @@
+const cacheName = 'komuza200-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/icon-512.png'
+];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('lineup-cache-v1').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/style.css',
-        '/script.js',
-        '/manifest.json',
-        '/icons/icon-192.png',
-        '/icons/icon-512.png'
-      ]);
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
     })
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
