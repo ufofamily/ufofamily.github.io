05eb378d896d61faf2c8afbadc46342e45b0da0b
ufofamily <60894835+ufofamily@users.noreply.github.com>
2025-07-12 23:44:35 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('lineup-cache-v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/script.js',
+        '/manifest.json',
+        '/icons/icon-192.png',
+        '/icons/icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
